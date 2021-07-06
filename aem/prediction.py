9919aebdf5a2133b09909fee9726027c84b72e61
Sudipta Basak <basaks@gmail.com@users.noreply.github.com>
2021-07-06 15:57:54 +1000

    bug fix - index prediction df before concating

--- aem/prediction.py
+++ aem/prediction.py
@@ -8,10 +8,11 @@ def add_pred_to_data(X: pd.DataFrame, conf: Config, model) -> pd.DataFrame:
     model_cols = utils.select_columns_for_model(conf)
     if hasattr(model, 'predict_dist'):
         p, v, ql, qu = model.predict_dist(X[model_cols], interval=conf.quantiles)
-        X = pd.concat((X, pd.DataFrame({'pred': p, 'variance': v, 'lower_quantile': ql, 'upper_quantile': qu})),
+        X = pd.concat((X, pd.DataFrame({'pred': p, 'variance': v, 'lower_quantile': ql, 'upper_quantile': qu}, index=X.index)),
                       axis=1)
         log.info("Added prediction, variance and quantiles to output dataframe")
     else:
         p = model.predict(X[model_cols])
-        X = pd.concat((X, pd.DataFrame({'pred': p})), axis=1)
+        X = pd.concat((X, pd.DataFrame({'pred': p}, index=X.index)), axis=1)
+        log.info("Added prediction to output dataframe")
     return X
