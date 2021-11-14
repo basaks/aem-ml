6ba2b6ca07fee0c28d874ca2581b845d34ea86dc
Sudipta Basak <basaks54321@users.noreply.github.com>
2021-11-15 00:35:09 +1100

    intersect raster script + resample option to reduce density

--- scripts/intersect_rasters.py
+++ scripts/intersect_rasters.py
@@ -1,7 +1,7 @@
 from pathlib import Path
+import numpy as np
 import rasterio
 import geopandas as gpd
-from sklearn.cluster import DBSCAN
 from joblib import Parallel, delayed
 
 data_location = \
@@ -9,7 +9,11 @@ data_location = \
 # Read points from shapefile
 
 shapefile_location = Path("/g/data/ge3/aem_sections/AEM_covariates/")
+
+# local
+# k = data_location.joinpath('data', 'LATITUDE_GRID1.tif')
 # shapefile_location = Path("configs/data")
+# shp = shapefile_location.joinpath('geochem_sites.shp')
 
 geotifs = {
     "relief_radius4.tif": "relief4",
@@ -26,20 +30,42 @@ geotifs = {
 }
 
 
-ratio = 10  # keep 1 in 10
+downscale_factor = 2  # keep 1 point in a 2x2 cell
 
 
 def intersect_and_sample_shp(shp: Path):
     print("====================================\n", f"intersecting {shp.as_posix()}")
     pts = gpd.read_file(shp)
-    coords = [(p.x, p.y) for p in pts.geometry]
+    coords = np.array([(p.x, p.y) for p in pts.geometry])
+    tif_name = list(geotifs.keys())[0]
+    tif = data_location.joinpath(tif_name)
+    orig_cols = pts.columns
+    with rasterio.open(tif) as src:
+        # resample data to target shape
+        data = src.read(
+            out_shape=(
+                src.count,
+                int(src.height / downscale_factor),
+                int(src.width / downscale_factor)
+            ),
+            resampling=rasterio.enums.Resampling.bilinear
+        )
+        # scale image transform
+        transform = src.transform * src.transform.scale(
+            (src.width / data.shape[-1]),
+            (src.height / data.shape[-2])
+        )
+        pts["rows"], pts["cols"] = rasterio.transform.rowcol(transform, coords[:, 0], coords[:, 1])
+
+    pts_deduped = pts.drop_duplicates(subset=['rows', 'cols'])[orig_cols]
+    coords_deduped = np.array([(p.x, p.y) for p in pts_deduped.geometry])
+
     for k, v in geotifs.items():
         print(f"adding {k} to output dataframe")
-        src = rasterio.open(data_location.joinpath(k))
-        # Sample the raster at every point location and store values in DataFrame
-        pts[v] = [x[0] for x in src.sample(coords)]
-    pts.to_file(Path('out').joinpath(shp.name))
-    pts.to_csv(Path("out").joinpath(shp.stem + ".csv"), index=False)
+        with rasterio.open(data_location.joinpath(k)) as src:
+            pts_deduped[v] = [x[0] for x in src.sample(coords_deduped)]
+    pts_deduped.to_file(Path('out').joinpath(shp.name))
+    # pts.to_csv(Path("out").joinpath(shp.stem + ".csv"), index=False)
 
 
 rets = Parallel(
